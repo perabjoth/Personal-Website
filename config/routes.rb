107073979530449b07d267776c185a5c2e1fd32d
perabjoth <perabjothsinghbajwa@gmail.com>
2015-09-01 21:46:55 +0000

    Added projects and removed turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :projects
   resources :about_me
   resources :contact_me
   # The priority is based upon order of creation: first created -> highest priority.
